e008365ff011cbb36072e9a600889a3547d30782
Tim Burks <tim@radtastical.com>
2014-06-30 14:04:48 -0700

    improved error handling

--- deploy.go
+++ deploy.go
@@ -16,111 +16,143 @@ func (c Connection) performRequestIntoBuffer(req *http.Request) (buffer []byte, 
 	authorization := fmt.Sprintf("Basic %v", encoding)
 	req.Header.Add("Authorization", authorization)
 	response, err := client.Do(req)
-	check(err)
+	if err != nil {
+		return 
+	}
 	buffer, err = ioutil.ReadAll(response.Body)
-	check(err)
+	if err != nil {
+		return 
+	}
 	defer response.Body.Close()
 	if response.StatusCode != 200 {
 		fmt.Printf("status code %v\n", response.StatusCode)
 		fmt.Println(string(buffer))
 		panic("goodbye")
 	}
-	return
+	return 
 }
 
 func (c Connection) performRequestIntoJSON(result interface{}, req *http.Request) (err error) {
 	buffer, err := c.performRequestIntoBuffer(req)
+	if err != nil {
+		return err
+	}
 	err = json.Unmarshal(buffer, result)
-	return
+	return err
 }
 
-func (c Connection) GetApps(result *[]App) {
+func (c Connection) GetApps(result *[]App) (err error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%v/control/apps", c.Service), nil)
-	check(err)
+	if err != nil {
+		return err
+	}
 	buffer, err := c.performRequestIntoBuffer(req)
-	err = json.Unmarshal(buffer, result)
-	check(err)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buffer, result)
 }
 
-func (c Connection) CreateApp(result *map[string]interface{}, app App) {
+func (c Connection) CreateApp(result *map[string]interface{}, app App) (err error) {
 	jsonData, err := json.Marshal(app)
 	post_data := bytes.NewReader(jsonData)
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps", c.Service), post_data)
-	check(err)
+	if err != nil {
+		return err
+	}
 	req.Header.Add("Content-Type", "application/json")
-	c.performRequestIntoJSON(result, req)
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) DeleteApps(result *map[string]interface{}) {
+func (c Connection) DeleteApps(result *map[string]interface{}) (err error) {
 	req, err := http.NewRequest("DELETE", fmt.Sprintf("%v/control/apps", c.Service), nil)
-	check(err)
-	c.performRequestIntoJSON(result, req)
+	if err != nil {
+		return err
+	}
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) GetApp(result *App, appid string) {
+func (c Connection) GetApp(result *App, appid string) (err error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%v/control/apps/%v", c.Service, appid), nil)
-	check(err)
+	if err != nil {
+		return err
+	}
 	buffer, err := c.performRequestIntoBuffer(req)
-	err = json.Unmarshal(buffer, result)
-	check(err)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buffer, result)
 }
 
-func (c Connection) DeleteApp(result *map[string]interface{}, appid string) {
+func (c Connection) DeleteApp(result *map[string]interface{}, appid string) (err error) {
 	req, err := http.NewRequest("DELETE", fmt.Sprintf("%v/control/apps/%v", c.Service, appid), nil)
-	check(err)
-	err = c.performRequestIntoJSON(result, req)
-	check(err)
+	if err != nil {
+		return err
+	}
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) DeleteAppVersion(result *map[string]interface{}, appid string, versionid string) {
+func (c Connection) DeleteAppVersion(result *map[string]interface{}, appid string, versionid string) (err error) {
 	req, err := http.NewRequest("DELETE", fmt.Sprintf("%v/control/apps/%v/versions/%v", c.Service, appid, versionid), nil)
-	check(err)
-	c.performRequestIntoJSON(result, req)
+	if err != nil {
+		return err
+	}
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) CreateAppVersion(result *map[string]interface{}, appid string, version []byte) {
+func (c Connection) CreateAppVersion(result *map[string]interface{}, appid string, version []byte) (err error) {
 	post_data := bytes.NewReader(version)
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps/%v/versions", c.Service, appid), post_data)
-	check(err)
-	c.performRequestIntoJSON(result, req)
+	if err != nil {
+		return err
+	}
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) StartAppVersion(result *map[string]interface{}, appid string, versionid string) {
+func (c Connection) StartAppVersion(result *map[string]interface{}, appid string, versionid string) (err error) {
 	data := url.Values{}
 	data.Set("command", "start")
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps/%v/versions/%v", c.Service, appid, versionid),
 		bytes.NewBufferString(data.Encode()))
-	check(err)
+	if err != nil {
+		return err
+	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	c.performRequestIntoJSON(result, req)
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) StopAppVersion(result *map[string]interface{}, appid string, versionid string) {
+func (c Connection) StopAppVersion(result *map[string]interface{}, appid string, versionid string) (err error) {
 	data := url.Values{}
 	data.Set("command", "stop")
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps/%v/versions/%v", c.Service, appid, versionid),
 		bytes.NewBufferString(data.Encode()))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	check(err)
-	c.performRequestIntoJSON(result, req)
+	if err != nil {
+		return err
+	}
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) StartApp(result *map[string]interface{}, appid string) {
+func (c Connection) StartApp(result *map[string]interface{}, appid string) (err error) {
 	data := url.Values{}
 	data.Set("command", "start")
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps/%v", c.Service, appid),
 		bytes.NewBufferString(data.Encode()))
-	check(err)
+	if err != nil {
+		return err
+	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	c.performRequestIntoJSON(result, req)
+	return c.performRequestIntoJSON(result, req)
 }
 
-func (c Connection) StopApp(result *map[string]interface{}, appid string) {
+func (c Connection) StopApp(result *map[string]interface{}, appid string) (err error) {
 	data := url.Values{}
 	data.Set("command", "stop")
 	req, err := http.NewRequest("POST", fmt.Sprintf("%v/control/apps/%v", c.Service, appid),
 		bytes.NewBufferString(data.Encode()))
-	check(err)
+	if err != nil {
+		return err
+	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	c.performRequestIntoJSON(result, req)
+	return c.performRequestIntoJSON(result, req)
 }
